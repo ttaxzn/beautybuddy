be4c72d6bebf17dc2f8418c396281a2588d06f0a
ttaxzn <ttaxzn@umsystem.edu>
2023-09-05 22:53:11 -0500

    AUTH0

--- beautybuddy/src/App.js
+++ beautybuddy/src/App.js
@@ -1,12 +1,21 @@
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+import "./App.css";
 
 function App() {
   return (
     <main>
       <h1>Auth0 Login</h1>
-      <LoginButton />
-      <LogoutButton />
+      <div className="wrapper">
+        <div className="homeWrapper">Home</div>
+        <div className="otherWrapper">
+          <LoginButton />
+          <LogoutButton />
+        </div>
+      </div>
+
+      <Profile />
     </main>
   );
 }
--- beautybuddy/src/components/LoginButton.js
+++ beautybuddy/src/components/LoginButton.js
@@ -1,11 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
-  const { loginwithRedirect, isAuthenticated } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
 
   return (
     !isAuthenticated && (
-      <button onClick={() => loginwithRedirect()}>Sign In</button>
+      <button onClick={() => loginWithRedirect()}>Sign In</button>
     )
   );
 };
